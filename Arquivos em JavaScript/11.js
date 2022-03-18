47f66395fe701cc49fcd994723dc9c123e7e8ee0
João Conrado <99970376+JoaoSConrado@users.noreply.github.com>
2022-03-17 21:33:21 -0300

    Delete Arquivos em JavaScript directory

--- Arquivos em JavaScript/11.js
+++
@@ -1,8 +0,0 @@
-        var odômetroInicioDia       = parseFloat(prompt("Digite a marcação do odômetro(Km) no início do dia:"));
-        var odômetroFimDia          = parseFloat(prompt("Digite a marcação do odômetro(Km) no fim do dia:"));
-        var litrosCombustivel       = parseFloat(prompt("Digite o número de litros de combustível gasto:"))
-        var valorPassageiros        = parseFloat(prompt("valor total(R$) recebido dos passageiros:"))
-
-        document.write ("A média do consumo diário foi: " + ((odômetroFimDia - odômetroInicioDia)/litrosCombustivel) + "Km/L");
-
-        document.write (" <br> <br> O Lucro Líquido diário foi: " + "R$ " +(valorPassageiros - (litrosCombustivel * 2.90)));
--- Arquivos em JavaScript/16.js
+++
@@ -1,9 +0,0 @@
-        var x = parseInt(prompt("Digite o o número de maçãs compradas: "));
-
-        if (x < 12) {
-            document.write ("O valor total da compra é: " + "R$ " + (0.30 * x))
-        }
-
-        if (x >= 12) {
-            document.write ("O valor total da compra é: " + "R$ " + (0.25 * x))
-        }
